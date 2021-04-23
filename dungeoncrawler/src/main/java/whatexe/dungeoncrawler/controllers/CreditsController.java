7473d76c398b3b24317e3c7e6d653177cc5ee3e5
rnewton31 <rnewton31@gatech.edu>
2021-04-23 19:29:39 -0400

    Finalized config, main menu, and credits

---
+++ dungeoncrawler/src/main/java/whatexe/dungeoncrawler/controllers/CreditsController.java
@@ -0,0 +1,33 @@
+package whatexe.dungeoncrawler.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import whatexe.dungeoncrawler.MainApp;
+import whatexe.dungeoncrawler.ManagedController;
+
+import java.io.IOException;
+
+public class CreditsController extends ManagedController {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void postInit() {
+        MainApp.addKeyPressHandler(e -> {
+            if (e.getCode() == KeyCode.ESCAPE) {
+                try {
+                    MainApp.switchScene("Main Menu");
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @FXML
+    protected void toMenu() throws IOException {
+        MainApp.switchScene("Main Menu");
+    }
+}
